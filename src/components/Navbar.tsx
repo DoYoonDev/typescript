6f63705637c53150bfc6ae288c7dadda4dec22e3
LeeJongHyun-Raymond <dlwhdgus482@gmail.com>
2025-06-08 04:17:52 +0900

    로그인 과제

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,77 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import {
+    Avatar,
+    Box,
+    IconButton,
+    Menu,
+    MenuItem,
+    styled,
+    useMediaQuery,
+} from '@mui/material'
+import React, { useState } from 'react'
 import LoginButton from '../common/components/LoginButton'
+import useGetCurrentUserProfile from '../hooks/useGetCurrentUserProfile'
+import { useQueryClient } from '@tanstack/react-query';
+
+const ProfileContainer = styled("div")({
+    display: "flex",
+    alignItems: "center",
+    cursor: "pointer",
+    borderRadius: "8px",
+});
+
+const ProfileMenu = styled(Menu)({
+    "& .MuiPaper-root": {
+        color: "white",
+        minWidth: "160px",
+    },
+});
+
+const ProfileMenuItem = styled(MenuItem)({
+    "&:hover": {
+        backgroundColor: "#444",
+    },
+});
 
 const Navbar = () => {
+    const { data: userProfile } = useGetCurrentUserProfile();
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const queryClient = useQueryClient();
+
+    const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const logout = () => {
+        localStorage.removeItem('access_token');
+        queryClient.removeQueries({ queryKey: ['current-user-profile'], exact: true });
+        handleMenuClose();
+    };
     return (
         <Box display="flex" justifyContent="flex-end" alignItems="center" height="64px">
-            <LoginButton />
+            {userProfile ? (
+                <ProfileContainer>
+                    <IconButton onClick={handleMenuOpen} size="small">
+                        <Avatar
+                            src={userProfile.images[0]?.url}
+                            alt={userProfile.display_name}
+                        />
+                    </IconButton>
+                    <ProfileMenu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                        keepMounted
+                    >
+                        <ProfileMenuItem onClick={logout}>Log out</ProfileMenuItem>
+                    </ProfileMenu>
+                </ProfileContainer>
+            ) : (
+                <LoginButton />
+            )}
         </Box>
     )
 }
--- src/models/commonType.ts
+++ src/models/commonType.ts
@@ -7,5 +7,15 @@ export interface Image {
     width: number | null
 }
 export interface Restriction {
-    reason:string
+    reason: string
+}
+
+export interface Followers {
+    href?: string;
+    total?: number;
+}
+
+export interface ExplictContent {
+    filter_enabled?: boolean;
+    filter_locked?: boolean;
 }
